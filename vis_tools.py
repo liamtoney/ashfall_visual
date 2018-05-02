0c91ac36895084af90e76f193d02fb23490beff8
liamtoney <toney.liam@gmail.com>
2018-05-02 01:11:41 +0000

    update netCDF and geo feature functions and adjust notebooks accordingly

--- vis_tools.py
+++ vis_tools.py
@@ -1,91 +1,39 @@
-# MODULE containing foundational tools for ashfall forecast visualization
+# various ashfall visualization utility functions
 
-import os  # for running GMT commands
-import xarray as xr  # for reading netCDF files
 import numpy as np
+import xarray as xr
+import cartopy.feature as cf
 
-# This function is from Chai
-# https://github.com/ccp137/DynamicViz/blob/master/utility.py
-def read_gmt_boundary(filename):
-    '''
-    Read boundary data from text files that are extracted by GMT
+def read_hysplit_netcdf(filename):   
+    """Reads a HYSPLIT netCDF file.
     
-    Input:
-        filename is the filename for the boundary data file
+    Reads the netCDF file, crops the model, adds an "empty grid" time step at t=0,
+    and adds in volcano location info.
+        
+    Args:
+        filename: A string specifying the path/filename for the netCDF file.
     
-    Output:
-        lat_list is a list of latitudes
-        lon_list is a list of longitude
+    Returns:
+        model: An xarray.Dataset containing all of the processed netCDF info.
+    """
     
-    '''
-    fid = open(filename,'r')
-    lon_list, lat_list = [], []
-    temp_lon, temp_lat = [], []
-    for aline in fid:
-        words = aline.split()
-        if words[0] != '>':
-            temp_lon.append(float(words[0]))
-            temp_lat.append(float(words[1]))
-        else:
-            lon_list.append(temp_lon)
-            lat_list.append(temp_lat)
-            temp_lon, temp_lat = [], []
-    fid.close()
-    lon_list.append(temp_lon)
-    lat_list.append(temp_lat)
-    return lat_list, lon_list
-
-def grab_gmt_features(latmin, latmax, lonmin, lonmax, res):
-    '''
-    Grab geographical feature data from GMT for a specified extent and resolution
-    Requires the os module
-    Also requires GMT to be installed, obviously...
+    model = xr.open_dataset(filename)
     
-    Input:
-        latmin, latmax, lonmin, lonmax specify extent in decimal degrees
-        res (a string) can be either f, h, i, l, or c depending upon desired resolution
+    model = model.isel(lon=slice(-1))  # remove all model grid points with longitude = 180.0 degrees
     
-    Output:
-        feature_data is a dictionary containing coordinates of geo features (each feature is a list)
+    final_deposition = model.isel(time=-1)['total_deposition'].values  # the "final" ash distribution
+    coords = np.argwhere(final_deposition > 0)
+    y_min, x_min = coords.min(axis=0)
+    y_max, x_max = coords.max(axis=0)
+    model = model.isel(lon=slice(x_min, x_max+1), lat=slice(y_min, y_max+1))  # crop model
     
-    '''
-    # the projection (-J) info shouldn't make a difference here
-    cmd = 'gmt pscoast -JB{0}/{2}/{2}/{3}/4.7i -R{0}/{1}/{2}/{3} -D{4} -W -M > temp.xy'.format(lonmin, lonmax,
-                                                                                               latmin, latmax, res)
-    os.system(cmd)
+    np.place(model['total_deposition'].values, model['total_deposition'].values == 0, np.nan)
     
-    feature_data = {}
-    feature_data['latitude'], feature_data['longitude'] = read_gmt_boundary('temp.xy')
+    # add in an extra, empty time step at t=0
+    model = xr.concat((model.isel(time=0), model), dim='time')
+    model.isel(time=0)['total_deposition'].values.fill(np.nan)
+    model['time'].values[0] = np.datetime64(model.attrs['eruption_time'])
     
-    # clean up
-    os.system('rm gmt.history')
-    os.system('rm temp.xy')
-    
-    return feature_data
-
-def read_hysplit_netcdf(filename):
-    '''
-    Read a netCDF file, add in volcano location info, and add an "empty grid" time step at t=0
-    Requires the xarray package
-    
-    Input:
-        filename is the filename and path for the netCDF file
-    
-    Output:
-        model is an xarray.Dataset with all the netCDF info 
-    
-    '''
-#     # taken from https://volcano.si.edu/search_volcano.cfm
-#     src_locs = {'auckland':[-36.9, 174.87],\
-#                 'mayor':[-37.28, 176.25],\
-#                 'white':[-37.52, 177.18],\
-#                 'haroharo':[-38.12, 176.5],\
-#                 'tarawera':[-38.12, 176.5],\
-#                 'taupo':[-38.82, 176],\
-#                 'tongariro':[-39.157, 175.632],\
-#                 'ngauruhoe':[-39.157, 175.632],\
-#                 'ruapehu':[-39.28, 175.57],\
-#                 'taranaki':[-39.3, 174.07]}
     # taken from ASHFALL model output files (converted from NZMG to WGS84)
     src_locs = {'auckland':[-36.8882, 174.7352],\
                 'mayor':[-37.2852, 176.2562],\
@@ -97,19 +45,34 @@ def read_hysplit_netcdf(filename):
                 'ngauruhoe':[-39.1552, 175.6322],\
                 'ruapehu':[-39.2816, 175.5639],\
                 'taranaki':[-39.2952, 174.0642]}
-    volc_name = filename.split('_')[1]
-    
-    # open model and add in source location info
-    model = xr.open_dataset(filename)
+    volc_name = filename.split('/')[-1].split('_')[1]
     model.attrs['volcano_location'] = src_locs[volc_name]
     
-    # create an empty grid at t=0
-    empty = model.isel(time=0)
-    empty['total_deposition'].values.fill(0)
-    empty['time'] = np.datetime64(model.attrs['eruption_time'])
-
-    # insert the grid into the model at t=0
-    model = xr.concat((empty, model), dim='time')
-    model = model.transpose('time', 'lat', 'lon')
-
     return model
+
+def grab_gshhg_features(scale, levels, extent):
+    """Grabs lat/lon coordinates for GSHHG features.
+    
+    Grab geographical feature data from the GSHHG database for a specified extent,
+    level of detail, and resolution. Outputs simple lat/lon coordinates -- useful
+    for plotting backends (like Bokeh) that don't have fancy geo feature integration.
+    
+    Args:
+        scale: A string, can be either f(ull), h(igh), i(ntermediate), l(ow), or c(rap) depending upon desired resolution.
+        levels: Specify which level(s) of feature to plot; [1] is only coastlines and [1, 2, 3, 4] is everything.
+        extent: Specify [lonmin, lonmax, latmin, latmax] in decimal degrees.
+    
+    Returns:
+        features: A dictionary containing coordinates of geo features (WGS84 lat/lon, decimal degrees).
+    """
+    
+    unformatted_features = list(cf.GSHHSFeature(scale=scale, levels=levels).intersecting_geometries(extent))
+
+    features = {'latitude':[], 'longitude':[]}
+    for feature in unformatted_features:
+        lons = list(list(feature)[0].exterior.coords.xy[0])
+        lats = list(list(feature)[0].exterior.coords.xy[1])
+        features['longitude'].append(lons)
+        features['latitude'].append(lats)
+    
+    return features
